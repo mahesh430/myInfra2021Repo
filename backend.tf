9dcaf06015db370c2e16fd409ceb7feae33cc218
mahesh430 <umamahesh690@gmail.com>
2022-03-27 09:31:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-terraform-state-bkt"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "terraform-state-lock"
   }
 }
